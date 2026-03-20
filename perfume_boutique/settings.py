d3bcf449523e4b8a1a22a95954d3d59eb8a47a75
Sergo <sergo.inasaridze@gmail.com>
2026-03-20 20:03:52 +0400

    update

--- perfume_boutique/settings.py
+++ perfume_boutique/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-your-secret-key-here'  # Измените на случайный
+SECRET_KEY = os.getenv(|"SECRET_KEY", "local-secret-key")
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["parfumcity.onrender.com", 'localhost', "127.0.0.1" ]
 
 # Добавляем наше приложение
 INSTALLED_APPS = [
