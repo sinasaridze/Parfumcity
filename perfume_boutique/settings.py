97794bb45ec69e1b8581fb295fe8d3e252e76bea
sinasaridze <s.inasaridze@upturnlab.com>
2026-03-20 20:40:34 +0400

    Update settings.py

--- perfume_boutique/settings.py
+++ perfume_boutique/settings.py
@@ -79,10 +79,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Статические файлы
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Добавляем эту строку
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Медиа файлы
 MEDIA_URL = '/media/'
